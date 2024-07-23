0ad82f45a7c33946a2867a5caffcf26f29ebd7b1
wongsitu <wongsitu@ksu.edu>
2024-07-23 09:11:02 -0600

    amplify

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -23,15 +23,28 @@ export class TaskService {
 
   constructor(private http: HttpClient) {
     this.tasksSubject$.subscribe((tasks) => {
-      this._plannedCountSource.next(tasks.filter((task) => task.state === 'Planned').reduce((acc, task) => acc + task.estimate, 0));
-      this._inProgressCountSource.next(tasks.filter((task) => task.state === 'InProgress').reduce((acc, task) => acc + task.estimate, 0));
-      this._completedCountSource.next(tasks.filter((task) => task.state === 'Completed').reduce((acc, task) => acc + task.estimate, 0));
-    })
+      this._plannedCountSource.next(
+        tasks
+          .filter((task) => task.state === 'Planned')
+          .reduce((acc, task) => acc + task.estimate, 0)
+      );
+      this._inProgressCountSource.next(
+        tasks
+          .filter((task) => task.state === 'InProgress')
+          .reduce((acc, task) => acc + task.estimate, 0)
+      );
+      this._completedCountSource.next(
+        tasks
+          .filter((task) => task.state === 'Completed')
+          .reduce((acc, task) => acc + task.estimate, 0)
+      );
+    });
   }
 
   getTasks() {
-    return this.http.get('/tasks')
-      .pipe(map(response => TaskListSchema.parse(response)))
+    return this.http
+      .get('/tasks')
+      .pipe(map((response) => TaskListSchema.parse(response)))
       .subscribe({
         next: (data) => {
           this._tasksSubjectSource.next(data);
@@ -39,52 +52,59 @@ export class TaskService {
         error: (error: ZodError['errors']) => {
           console.error('There was an error!', error);
         },
-      })
+      });
   }
 
   createTask(task: Task) {
     const payload = TaskSchema.parse(task);
-    return this.http.post('/tasks', payload)
-      .pipe(map(response => TaskSchema.parse(response)))
+    return this.http
+      .post('/tasks', payload)
+      .pipe(map((response) => TaskSchema.parse(response)))
       .subscribe({
         next: (data) => {
-          const prevState = this._tasksSubjectSource.getValue()
+          const prevState = this._tasksSubjectSource.getValue();
           this._tasksSubjectSource.next([...prevState, data]);
         },
         error: (error: ZodError['errors']) => {
           console.error('There was an error!', error);
         },
-      })
+      });
   }
 
   deleteTask(task: Task) {
-    return this.http.delete(`/tasks/${task.id}`)
-      .pipe(map(response => TaskSchema.parse(response)))
+    return this.http
+      .delete(`/tasks/${task.id}`)
+      .pipe(map((response) => TaskSchema.parse(response)))
       .subscribe({
         next: (data) => {
-          const prevState = this._tasksSubjectSource.getValue()
-          this._tasksSubjectSource.next(prevState.filter((t) => t.id !== data.id));
+          const prevState = this._tasksSubjectSource.getValue();
+          this._tasksSubjectSource.next(
+            prevState.filter((t) => t.id !== data.id)
+          );
         },
         error: (error: ZodError['errors']) => {
           console.error('There was an error!', error);
         },
-      })
+      });
   }
 
   updateTask(task: Task) {
     const parsedTask = TaskSchema.parse(task);
-    const { id, ...rest } = parsedTask
+    const { id, ...rest } = parsedTask;
 
-    return this.http.put(`/tasks/${id}`, rest)
-      .pipe(map(response => TaskSchema.parse(response)))
+    return this.http
+      .put(`/tasks/${id}`, rest)
+      .pipe(map((response) => TaskSchema.parse(response)))
       .subscribe({
         next: (data) => {
-          const prevState = this._tasksSubjectSource.getValue()
-          this._tasksSubjectSource.next(prevState.map((t) => t.id === data.id ? data : t));
+          const prevState = this._tasksSubjectSource.getValue();
+          this._tasksSubjectSource.next(
+            prevState.map((t) => (t.id === data.id ? data : t))
+          );
         },
         error: (error: ZodError['errors']) => {
           console.error('There was an error!', error);
         },
-      })
+      });
   }
 }
